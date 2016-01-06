e5ec1e5f10bd04de0abe22ac2b8a775c7230154d
DukeOfTrunks <rx.duke@mail.ru>
2016-01-06 03:17:01 +0300

    MM-1: adding materialdesignlibrary dependency

--- src/main/java/com/dotdevcorp/android/HelloAndroidActivity.java
+++ src/main/java/com/dotdevcorp/android/HelloAndroidActivity.java
@@ -1,27 +1,30 @@
 package com.dotdevcorp.android;
 
-import android.app.Activity;
-import android.os.Bundle;
 import android.view.Menu;
+import com.blunderer.materialdesignlibrary.activities.Activity;
+import com.blunderer.materialdesignlibrary.handlers.ActionBarDefaultHandler;
+import com.blunderer.materialdesignlibrary.handlers.ActionBarHandler;
 
 public class HelloAndroidActivity extends Activity {
 
-    /**
-     * Called when the activity is first created.
-     * @param savedInstanceState If the activity is being re-initialized after 
-     * previously being shut down then this Bundle contains the data it most 
-     * recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
-     */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected int getContentView() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected boolean enableActionBarShadow() {
+        return false;
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-	// Inflate the menu; this adds items to the action bar if it is present.
-	getMenuInflater().inflate(com.dotdevcorp.android.R.menu.main, menu);
-	return true;
+        getMenuInflater().inflate(com.dotdevcorp.android.R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    protected ActionBarHandler getActionBarHandler() {
+        return new ActionBarDefaultHandler(this);
     }
 }
